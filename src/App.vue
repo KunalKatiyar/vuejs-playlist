20a6890a3477ade90c7ab567e660feab7cc5f2ea
KunalKatiyar <katiyarkunal2011@gmail.com>
2020-08-14 15:42:35 +0530

    Nesting Components

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,22 @@
 <template>
   <div>
     <h1>{{ title }}</h1>
-    <p>{{ greeting() }}</p>
+    <ninjas></ninjas>
   </div>
 </template>
 
 <script>
+
+import Ninjas from './Ninjas.vue'
+
 export default {
+  components: {
+    'ninjas': Ninjas
+  },
   data () {
     return {
-      title: "Your first Vue file!"
+      title: "Ninja App"
     }
-  },
-  methods: {
-    greeting: function(){
-      return 'Lets Go!';
-    } 
   }
 }
 </script>
