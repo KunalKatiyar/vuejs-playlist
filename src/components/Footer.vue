859ec6705be394545585c87c953d95d1ce29727d
KunalKatiyar <katiyarkunal2011@gmail.com>
2020-08-15 10:09:53 +0530

    The Event Bus

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-
+import { bus } from '../main';
 export default {
   props: {
     title: {
@@ -16,6 +16,11 @@ export default {
     return {
         copyright: 'Copyright 2020'
     }
+  },
+  created(){
+    bus.$on('titleChanged', (data)=>{
+      this.title = data;
+    })
   }
 }
 </script>
